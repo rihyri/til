767df2a790a06229944e637fa78e9e314147659d
이혜리 <rihyri.dev@gmail.com>
2026-09-15 10:30:08 +0900

    chore: initial commit

---
+++ src/main/java/io/github/rihyri/til/TilApplication.java
@@ -0,0 +1,13 @@
+package io.github.rihyri.til;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilApplication.class, args);
+	}
+
+}
